ebe5aa1fcaa49164ee0b08dba5e6381342ccbc4f
airick94 <kovari.erik@gmail.com>
2019-01-25 21:18:17 +0000

    Started setting up the controller

---
+++ GameJam19/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    BoxCollider col;
+
+    void Start()
+    {
+        col = GetComponent<BoxCollider>)();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void CollectItem(Item item)
+    {
+
+    }
+
+    void OnTriggerEnter()
+    {
+
+    }
+}
